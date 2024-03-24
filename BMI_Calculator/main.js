cce00eb329db00504cf50598e10c51d9fe1fb3da
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 04:53:29 +0500

    Add files via upload

---
+++ BMI_Calculator/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightInKg = 70;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}`);
---
+++ BMI_Calculator/main.ts
@@ -0,0 +1,6 @@
+let weightInKg = 70;
+let heightInMeters = 1.75;
+
+let bmi = weightInKg / (heightInMeters * heightInMeters)
+
+console.log(`Your BMI is ${bmi}`);
