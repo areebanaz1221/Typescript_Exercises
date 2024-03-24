1c53a3c80bc4df16a23c4c85bcae22066f68f770
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 04:56:19 +0500

    Add files via upload

---
+++ Comparison_Operators/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 5;
+let b = 2;
+let isEqual = (a == b); // false
+let isNotEqual = (a != b); // true
+let isGreaterThan = (a > b); // true
+let isLessThan = (a < b);
+console.log(isLessThan);
---
+++ Comparison_Operators/main.ts
@@ -0,0 +1,6 @@
+let a = 5
+let b = 2
+let isEqual = ( a == b ); // false
+let isNotEqual = ( a != b ); // true
+let isGreaterThan = ( a > b ); // true
+let isLessThan = ( a < b ); // false
