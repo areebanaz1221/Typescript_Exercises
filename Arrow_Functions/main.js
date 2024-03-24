90b71fb0598152c2138bc7d62827b8428486eb4e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 04:50:21 +0500

    Add files via upload

---
+++ Arrow_Functions/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let halfFryEgg = (egg, butter, salt) => {
+    let cocked = 1 + 1.5 + 3;
+    return cocked;
+};
+let response = halfFryEgg(1, 1.5, 2);
+console.log(response);
---
+++ Arrow_Functions/main.ts
@@ -0,0 +1,7 @@
+let halfFryEgg = (egg: number, butter: number, salt: number): number => { 
+    
+    let cocked = 1 + 1.5 + 3;
+    return cocked
+};
+let response: number = halfFryEgg(1, 1.5, 2)
+console.log(response)
