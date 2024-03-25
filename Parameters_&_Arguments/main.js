9d894ca946c2c3ac0134756400325d291c8c3039
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:23:06 +0500

    Add files via upload

---
+++ Parameters_&_Arguments/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculateArea(width, height) {
+    return width * height;
+}
+let response = calculateArea(100, 50);
+console.log(response);
---
+++ Parameters_&_Arguments/main.ts
@@ -0,0 +1,8 @@
+
+function calculateArea(width: number, height: number): number {
+    return width * height;
+}
+
+let response: number = calculateArea(100, 50)
+
+console.log(response)
