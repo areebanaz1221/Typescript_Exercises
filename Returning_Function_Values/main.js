643e64afd0702f4d39cb5a79fe7f02db6d513734
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:26:59 +0500

    Add files via upload

---
+++ Returning_Function_Values/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function halfFryEgg() {
+    let cocked = 1 + 1.5 + 2;
+    //Egg + Butter + Salt
+    return cocked;
+}
+// Invoking the function
+let response = halfFryEgg();
+console.log(response);
---
+++ Returning_Function_Values/main.ts
@@ -0,0 +1,10 @@
+function halfFryEgg(): number {
+    let cocked = 1 + 1.5 + 2;
+    //Egg + Butter + Salt
+    return cocked
+}
+// Invoking the function
+
+let response: number = halfFryEgg()
+
+console.log(response)
