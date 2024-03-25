7ca5a7ff9cbe8608122be0bd47ee652ea37f6f03
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:09:26 +0500

    Add files via upload

---
+++ Exponentiation/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let layer = 5;
+let apple = 5;
+let power = layer ** 2;
+console.log(power); // 25
---
+++ Exponentiation/main.ts
@@ -0,0 +1,5 @@
+let layer: number = 5
+let apple: number = 5
+let power: number = layer ** 2;
+
+console.log(power) // 25
