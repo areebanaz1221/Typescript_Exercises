4abccf2891692161a73094120898f7fdd9854543
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:19:19 +0500

    Add files via upload

---
+++ Multiplication/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let bags = 4;
+let apples = 5;
+let totalApples = bags * apples;
+console.log(totalApples); //20
---
+++ Multiplication/main.ts
@@ -0,0 +1,5 @@
+let bags: number = 4;
+let apples: number = 5;
+let totalApples: number = bags * apples
+
+console.log(totalApples) //20
