2a07d2ccc8f130bf2f017dbcf45756875d31dbd9
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:17:54 +0500

    Add files via upload

---
+++ Modules2/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let apples = 5;
+let bags = 2;
+let reminder = apples % bags;
+console.log(reminder); // 1
---
+++ Modules2/main.ts
@@ -0,0 +1,5 @@
+let apples = 5
+let bags = 2
+let reminder = apples % bags;
+
+console.log(reminder) // 1
