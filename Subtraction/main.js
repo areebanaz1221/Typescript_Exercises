bdaae6d0ad9ee48d080facbc91214956e2d345b7
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:35:05 +0500

    Add files via upload

---
+++ Subtraction/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let cart = 5;
+let num3 = 2;
+let total = cart - num3;
+console.log(total); // 3
---
+++ Subtraction/main.ts
@@ -0,0 +1,5 @@
+let cart: number = 5;
+let num3: number = 2;
+let total: number = cart - num3
+
+console.log(total); // 3
