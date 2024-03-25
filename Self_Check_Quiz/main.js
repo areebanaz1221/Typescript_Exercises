3b289dd37e55b2d1903db79a45e3e6cf83bbfee3
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 18:29:55 +0500

    Add files via upload

---
+++ Self_Check_Quiz/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let answer = "correct";
+if (answer === "correct") {
+    console.log("You got it right!");
+}
+else {
+    console.log("Sorry, that's not correct.");
+}
---
+++ Self_Check_Quiz/main.ts
@@ -0,0 +1,6 @@
+let answer: string = "correct"
+if (answer === "correct") {
+     console.log("You got it right!");
+} else {
+    console.log("Sorry, that's not correct.");
+}
