9056bdc78df99cfc1c7562b6c42b5af5a67e79bc
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:06:16 +0500

    Add files via upload

---
+++ Else_If_Statements/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let weather = "cloudy";
+if (weather === "raining") {
+    console.log("Wear a raincoat.");
+}
+else if (weather === "cloudy") {
+    console.log("Wear a light jacket.");
+}
+else {
+    console.log("Wear a sunglasses.");
+}
---
+++ Else_If_Statements/main.ts
@@ -0,0 +1,8 @@
+let weather = "cloudy";
+if (weather === "raining") {
+    console.log("Wear a raincoat.");
+} else if (weather === "cloudy") {
+    console.log("Wear a light jacket.");
+} else {
+    console.log("Wear a sunglasses.");
+}
