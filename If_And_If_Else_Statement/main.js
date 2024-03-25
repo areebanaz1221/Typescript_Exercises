67a21f18979fce8d33be84a50b970af2b26040c0
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:21:42 +0500

    Add files via upload

---
+++ If_And_If_Else_Statement/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let itsRaining = false;
+if (itsRaining) {
+    console.log("Wear a raincoat.");
+}
+else {
+    console.log("Wear sunglasses");
+}
+console.log(itsRaining);
---
+++ If_And_If_Else_Statement/main.ts
@@ -0,0 +1,8 @@
+let itsRaining = false;
+if (itsRaining) {
+    console.log("Wear a raincoat.");
+} else {
+    console.log("Wear sunglasses");
+}
+
+console.log(itsRaining)
