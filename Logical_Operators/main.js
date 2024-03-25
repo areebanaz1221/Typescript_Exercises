06e8cec7298f87ebaf847dc6c6af2bfb1edc594f
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:24:01 +0500

    Add files via upload

---
+++ Logical_Operators/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 5;
+let b = 2;
+let logicalAnd = (a > 0) && (b > 0); // true
+let logicalOr = (a < 0) || (b > 0); // true
+let logicalNot = !(a > 0); // false
+console.log(logicalNot);
