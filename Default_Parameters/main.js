012a13d18da28405e746a331638c2a6417c4bb49
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:04:24 +0500

    Add files via upload

---
+++ Default_Parameters/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function halfFryEgg(egg = 1, butter = 1.5, salt = 2) {
+    return egg + butter + salt;
+}
+let responce = halfFryEgg(1, 1.5, 2);
+console.log(responce);
---
+++ Default_Parameters/main.ts
@@ -0,0 +1,8 @@
+function halfFryEgg(
+    egg: number = 1, butter: number = 1.5, salt: number = 2
+): number {
+    return egg + butter + salt;
+}
+let responce: number = halfFryEgg(1, 1.5, 2)
+
+console.log(responce)
