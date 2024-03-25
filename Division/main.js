be132789615bbf112c4d03b898f4fa2146c2beed
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-25 05:05:17 +0500

    Add files via upload

---
+++ Division/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let totalApples = 20;
+let bags = 4;
+let eachBags = totalApples / bags;
+console.log(eachBags); // 5
---
+++ Division/main.ts
@@ -0,0 +1,5 @@
+let totalApples: number = 20;
+let bags: number = 4;
+let eachBags: number = totalApples / bags
+
+console.log(eachBags) // 5
